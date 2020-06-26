8a0645ea0f13851c0edf1e7e8d74d2f778e186a9
Justin Sousa <jsousa1919@gmail.com>
2020-06-26 20:01:08 +0000

    Just having some fun :)

--- app/scripts/home.jsx
+++ app/scripts/home.jsx
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import classnames from 'classnames';
 
 import style from '../styles/home.scss';
 
 class ZencareApp extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      fakeClicked: false,
+    };
+  }
+
+  onFakeClick() {
+    this.setState(() => ({
+      fakeClicked: true,
+    }));
   }
 
   render() {
+    const {
+      fakeClicked,
+    } = this.state;
+
+    const fakeClasses = classnames(
+      'zencare-app__fake',
+      {
+        'zencare-app__fake--clicked': fakeClicked,
+      }
+    );
+
     return (
       <div className="zencare-app">
-        Zencare App
+        <div className="zencare-app__placeholder">
+          Zencare App
+        </div>
+        <div className={fakeClasses}>
+          <div onClick={() => this.onFakeClick()} className="btn zencare-app__fake__button">
+            Click Me!
+          </div>
+          <div className="zencare-app__fake__msg">
+            No Peeking!
+          </div>
+        </div>
       </div>
     );
   }
